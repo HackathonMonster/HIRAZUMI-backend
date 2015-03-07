acfda0305727c7e17a2bd336cf81cfeebeb11ae7
Yoshiyuki Taniguchi <iwatesoftware@live.jp>
2015-03-07 13:05:09 +0900

    initial commit

---
+++ Hirazumi/DataObjects/Sample.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hirazumi.DataObjects
+{
+    public class Sample : EntityData
+    {
+        public string Text { get; set; }
+    }
+}
